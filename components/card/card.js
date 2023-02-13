15430ae675863bf0ac3535c78b4e07bff3d4fbf1
Ana-teston <anapaulateston@gmail.com>
2023-02-13 11:44:03 +0100

    start to using the data from youtube

--- components/card/card.js
+++ components/card/card.js
@@ -8,7 +8,8 @@ import { motion } from "framer-motion";
 const Card = (props) => {
     const {
         imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
-        size = "medium"
+        size = "medium",
+        id
     } = props;
 
     const [imgSrc, setImgSrc]  = useState(imgUrl);
@@ -24,10 +25,11 @@ const Card = (props) => {
         setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80");
     };
 
+    const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
+
     return (
         <div>
-            Card
-            <motion.div className={cls(styles.imgMotionWrapper, classMap[size])} whileHover={{scale: 1.2}} >
+            <motion.div className={cls(styles.imgMotionWrapper, classMap[size])} whileHover={{...scale}} >
                 <Image
                     src={imgSrc}
                     alt="image"
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
-import Image from "next/legacy/image";
 import styles from '../styles/Home.module.css'
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
-import Card from "../components/card/card";
+import SectionCards from "../components/card/section-cards";
+import { getVideos } from "../lib/videos";
 
 export default function Home() {
+
+  const disneyVideos = getVideos();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,9 +22,11 @@ export default function Home() {
             subtitle='He is a smart dog'
             imgUrl='/static/photo.jpg'
         />
-        <Card imgUrl={'/static/photo.jpg'} size={"large"} />
-        <Card  size={"medium"} />
-        <Card imgUrl={'/static/photo.jpg'} size={"small"} />
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large"/>
+          <SectionCards title="Disney" videos={disneyVideos} size="medium"/>
+          <SectionCards title="Disney" videos={disneyVideos} size="small"/>
+        </div>
     </div>
   )
 }
